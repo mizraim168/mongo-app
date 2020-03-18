80d7d02d98c023afedf01891cfdb0c513ecd10ad
Adrian <rafael.avendano12317@gmail.com>
2020-03-17 21:09:07 -0600

    Agregando script para listings

--- Scripts/listings-example.js
+++ Scripts/listings-example.js
@@ -1,1 +1,156 @@
 // Put listings information here
+{
+	"name": "Bright and Airy",
+	"price": 1190,
+	"property_type": "Department",
+	"id_customer":0
+
+}
+{
+	"name": "Sun-drenched",
+	"price": 898,
+	"property_type": "Department",
+	"id_customer":1
+
+}
+{
+	"name": "Modernn beatiful bedford",
+	"price": 1067,
+	"property_type": "Department",
+	"id_customer":2
+
+}
+{
+	"name": "Pop house",
+	"price": 1617,
+	"property_type": "Department",
+	"id_customer":3
+
+}
+{
+	"name": "Flatbush Hideaway",
+	"price": 1275,
+	"property_type": "Department",
+	"id_customer":4
+
+}
+
+
+
+
+
+
+{
+	"name": "The small chateau in the trees",
+	"price": 1648,
+	"property_type": "House",
+	"id_customer":5
+
+}
+{
+	"name": "Exposed brick",
+	"price": 1998,
+	"property_type": "House",
+	"id_customer":6
+
+}
+{
+	"name": "Large 2 bedroom midtown",
+	"price": 2784,
+	"property_type": "House",
+	"id_customer":7
+
+}
+{
+	"name": "Modern condo",
+	"price": 1100,
+	"property_type": "House",
+	"id_customer":8
+
+}
+{
+	"name": "Posh junior",
+	"price": 100,
+	"property_type": "House",
+	"id_customer":9
+
+}
+
+
+
+
+{
+	"name": "Browstone studio",
+	"price": 2694,
+	"property_type": "Room",
+	"id_customer":10
+
+}
+{
+	"name": "Chic designer",
+	"price": 1460,
+	"property_type": "Room",
+	"id_customer":0
+
+}
+{
+	"name": "Classic brownstone",
+	"price": 1998,
+	"property_type": "Room",
+	"id_customer":1
+
+}
+{
+	"name": "Studio Apartament",
+	"price": 2021,
+	"property_type": "Room",
+	"id_customer":2
+
+}
+{
+	"name": "Traveller´s Flat",
+	"price": 1325,
+	"property_type": "Room",
+	"id_customer":3
+
+}
+
+
+
+
+
+{
+	"name": "Premium penthouse",
+	"price": 788,
+	"property_type": "PentHouse",
+	"id_customer":4
+
+}
+{
+	"name": "PentHouse at the beach",
+	"price": 1490,
+	"property_type": "PentHouse",
+	"id_customer":5
+
+}
+{
+	"name": "1001 PentHouse",
+	"price": 665,
+	"property_type": "PentHouse",
+	"id_customer":6
+
+}
+{
+	"name": "Snoopy´s PentHouse",
+	"price": 1152,
+	"property_type": "PentHouse",
+	"id_customer":7
+
+}
+{
+	"name": "penthouse Seaview",
+	"price": 3368,
+	"property_type": "PentHouse",
+	"id_customer":8
+
+}
