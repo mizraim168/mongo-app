7c683bbc8db1c86b0ad88eb0063390910625290d
Adrian <rafael.avendano12317@gmail.com>
2020-03-17 20:35:30 -0600

    Ingresando script de ejemplo para customers

--- Scripts/customers-example.js
+++ Scripts/customers-example.js
@@ -1,1 +1,209 @@
 // Put customer information here
+
+{
+
+	"address": "Carlos A. Madrazo",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Jose",
+	"LastName": "Mendez",
+	"status": "active"
+}
+
+
+{
+
+	"address": "El Tildio 106",
+	"city": "Guadalajara",
+	"country": "MX",
+	"district": "Zapopan",
+	"FirstName": "Miguel",
+	"LastName": "Ramírez",
+	"status": "active"
+}
+
+{
+
+	"address": "Las Fraguas 320",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Gustavo",
+	"LastName": "Morales",
+	"status": "active"
+}
+
+{
+
+	"address": "Ingenieros 345-369",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Franco",
+	"LastName": "Lopez",
+	"status": "active"
+}
+{
+
+	"address": "Viento LB",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Ana",
+	"LastName": "Mancera",
+	"status": "active"
+}
+{
+
+	"address": "Guillermo Prieto 501-523",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Eduardo",
+	"LastName": "Avendaño",
+	"status": "active"
+}
+{
+
+	"address": "Residencial del Valle I",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Maria",
+	"LastName": "Saltillo",
+	"status": "active"
+}
+{
+
+	"address": "Soria 157-225",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Sergio",
+	"LastName": "Mejorado",
+	"status": "active"
+}
+{
+
+	"address": "Garabato 126-120",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Jonathan",
+	"LastName": "Olivo",
+	"status": "active"
+}
+{
+
+	"address": "Cisne 306-202",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Rodrigo",
+	"LastName": "Brambila",
+	"status": "active"
+}
+{
+
+	"address": "Cádiz 99-185",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Ernesto",
+	"LastName": "Padilla",
+	"status": "active"
+}
+{
+
+	"address": "Torneros 406",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Maria",
+	"LastName": "Dominguez",
+	"status": "active"
+}
+{
+
+	"address": "Obrero Mundial 271-341",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Karla",
+	"LastName": "Montiel",
+	"status": "active"
+}
+{
+
+	"address": "Sauce 346",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Critian",
+	"LastName": "Meza",
+	"status": "active"
+}
+{
+
+	"address": "Lopez Mateos",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Ivan",
+	"LastName": "Femat",
+	"status": "active"
+}
+
+
+
+
+
+
+
+
+{
+	"address": "Prol. Libertad 1607",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Delia",
+	"LastName": "Magaña",
+	"status": "inactive"
+}
+{
+	"address": "Luis Chilardi 226-164",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Karime",
+	"LastName": "Olvera",
+	"status": "inactive"
+}
+{
+	"address": "Luis Chilardi 226",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Xiomara",
+	"LastName": "Carmona",
+	"status": "inactive"
+}
+{
+	"address": "Ponciano Arriaga 717",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Estela",
+	"LastName": "Molina",
+	"status": "inactive"
+}
+{
+	"address": "Dr. Alberto del Valle 813",
+	"city": "Aguascalientes",
+	"country": "MX",
+	"district": "Ags",
+	"FirstName": "Fernanda",
+	"LastName": "Arriaga",
+	"status": "inactive"
+}
